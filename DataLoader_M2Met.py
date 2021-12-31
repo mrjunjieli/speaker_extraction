088a2e00b999b30dda2db0a3b2084da7366ac9ed
李俊杰 <mrjunjieli@gmail.com>
2021-12-31 14:53:03 +0800

    Update DataLoader_M2Met.py

--- DataLoader_M2Met.py
+++ DataLoader_M2Met.py
@@ -5,7 +5,11 @@ from torch.serialization import load
 from torch.utils.data import Dataset,DataLoader
 import numpy as np 
 import random
-from simulation_room import simulation
+# from simulation_room import simulation
+import soundfile as sf
+import torch.nn as nn
+from Loss import cal_si_snr
+
 
 
 def handle_scp(scp_path):
@@ -51,11 +55,12 @@ def load_audio(index_dict,index,timeLen=3,sr=16000):
         key = keys[index]
     else:
         key = index 
-    audio_data,_ = librosa.load(index_dict[key],sr=sr) 
+    # audio_data,_ = librosa.load(index_dict[key],sr=sr) 
+    audio_data,_ = sf.read(index_dict[key])
     audio_data = audio_data.astype(np.float32)
 
     # audio_data,_ = librosa.load(index_dict[key],sr=sr) 
-    audio_data = np.expand_dims(audio_data,axis=0)
+    # audio_data = np.expand_dims(audio_data,axis=0)
     # audio_data = torch.tensor(audio_data,dtype=torch.float).unsqueeze(0)
 
     return audio_data 
@@ -78,7 +83,7 @@ class AudioDataset(Dataset):
     '''
     def __init__(self,s1_scp=None,stage='train',sr=16000):
         super(AudioDataset,self).__init__()
-        self.s1_audio = handle_scp(s1_scp)
+        self.audio = handle_scp(s1_scp)
         self.sr=sr
         self.speakername = get_allspeakername()
         self.speaker_audio = {}
@@ -88,7 +93,7 @@ class AudioDataset(Dataset):
             for line in p.readlines():
                 self.noise.append(line.strip())
 
-        for sp in self.s1_audio.keys():
+        for sp in self.audio.keys():
             spea = sp.strip().split(' ')[0]
             speaker = spea.split('-')[2]
             speaker = speaker.split('_')[-2:]
@@ -99,21 +104,32 @@ class AudioDataset(Dataset):
                 self.speaker_audio[speaker].append(sp)
 
     def __len__(self):
-        return len(self.s1_audio)
+        return len(self.audio)
     def __getitem__(self,index):
-        noise_path = random.choice(self.noise)
+        if self.stage=='train':
+            noise_scale = random.uniform(0,0.5)
+            noise_path = random.choice(self.noise)
+        if self.stage=='dev':
+            noise_scale=0.1
+            noise_path = self.noise[index%len(self.noise)]
+        noise_scale=1
         noise_data,_ = librosa.load(noise_path,sr=self.sr)
         noise_length = len(noise_data)
-        if self.stage=='train':
-            noise_data = noise_data[int(random.uniform(0,0.5)*noise_length):int(random.uniform(0.5,1)*noise_length)]
+        noise_data = noise_data[int(random.uniform(0,0.5)*noise_length):int(random.uniform(0.5,1)*noise_length)]
         noise_data = np.expand_dims(noise_data,0)
+        
+        noise_data *=noise_scale
+
         channel_num = random.choice([0,1,2,3,4,5,6,7])
-        s1_audio = load_audio(self.s1_audio,index,sr=self.sr)
+        # channel_num=0
+        s1_audio = load_audio(self.audio,index,sr=self.sr)
+        s1_audio = s1_audio[:,channel_num]
+        s1_audio = np.expand_dims(s1_audio,0)
+
         if s1_audio.shape[1]/self.sr>4:
             s1_audio = s1_audio[:,0:self.sr*4]
 
-        s1_audio = simulation(s1_audio,channel_num)
-        s1_id = self._returnspeakerName(self.s1_audio,index) #[0]name  [1]才是真实的id
+        s1_id = self._returnspeakerName(self.audio,index) #[0]name  [1]才是真实的id
         s1_name = s1_id[0].strip().split(' ')[0].split('-')[2].split('_')[-2:]
         s1_name = '_'.join(s1_name)
 
@@ -122,31 +138,39 @@ class AudioDataset(Dataset):
         elif self.stage=='dev': 
             ref_path = self.speaker_audio[s1_name][0]
 
-        ref_audio,_ = librosa.load(self.s1_audio[ref_path],sr=self.sr)
-        # ref_audio = np.expand_dims(ref_audio,axis=0)
-        ref_audio = simulation(ref_audio,random.choice([0,1,2,3,4,5,6,7]))
+        ref_audio,_ = sf.read(self.audio[ref_path])
+        if self.stage=='train':
+            ref_audio = ref_audio[:,random.choice([0,1,2,3,4,5,6,7])]
+        else:
+            ref_audio = ref_audio[:,0]
+        ref_audio = np.expand_dims(ref_audio,axis=0)
 
         if self.stage=='train':
-            num = random.choice([1,2,3,4])
+            num = random.choice([1,2,3])
             speaker_list = list(set(self.speaker_audio.keys())-set([s1_name]))
             speakers= random.sample(speaker_list,num)
+            end_length = int(s1_audio.shape[1]*random.uniform(0,0.05))
+            start_length = int(s1_audio.shape[1]*random.uniform(0,0.05))
         elif self.stage =='dev':
             speaker_list = list(self.speaker_audio.keys())
             speaker_index = speaker_list.index(s1_name)
-            num = speaker_index%4 +1
+            speaker_list = list(set(self.speaker_audio.keys())-set([s1_name]))
+            num = speaker_index%2 +1
             speakers = []
             while(num>0):
                 speakers.append(speaker_list[(speaker_index+num)%len(speaker_list)])
                 num-=1
 
-        end_length = int(s1_audio.shape[1]*random.uniform(0,0.05))
-        start_length = int(s1_audio.shape[1]*random.uniform(0,0.05))
+            end_length=0
+            start_length=0
+
         mix_audio = np.zeros((1,start_length+s1_audio.shape[1]+end_length))
 
         target_audio = np.pad(s1_audio,((0,0),(start_length,end_length)),"constant")
-        # print('t',target_audio.shape)
-        # print('m',mix_audio.shape)
-        target_audio = random.uniform(0.5,2) *target_audio
+        
+        if self.stage=='train':
+            target_audio = random.uniform(0.5,1.2) *target_audio
+
         mix_audio += target_audio
         length = mix_audio.shape[1]
 
@@ -156,12 +180,17 @@ class AudioDataset(Dataset):
             mix_audio[:,0:noise_data.shape[1]] += noise_data
 
         for speaker in speakers:
-            path = random.choice(self.speaker_audio[speaker])
-            path = self.s1_audio[path]
-            audio,_ = librosa.load(path,sr=self.sr)
-            # audio = np.expand_dims(audio,axis=0)
-            audio = simulation(audio,channel_num)
-            start_ = int(length*random.uniform(0,0.2))
+            if self.stage=='train':
+                path = random.choice(self.speaker_audio[speaker])
+                start_ = int(length*random.uniform(0,0.2))
+            if self.stage=='dev':
+                path = self.speaker_audio[speaker][0]
+                start_=0
+            path = self.audio[path]
+            audio,_ = sf.read(path)
+            audio = audio[:,channel_num]
+            audio = np.expand_dims(audio,axis=0)
+            
             left_length = length-start_
 
             if audio.shape[1]>left_length:
@@ -232,26 +261,50 @@ class AudioDataLoader(DataLoader):
 
 if __name__=='__main__':
     
-    audio_data_trainval = AudioDataset(s1_scp='/Work20/2020/lijunjie/AVModel/Conv_TasNet_speakemb/data/M2Met/train.scp',stage='train')
+    audio_data_trainval = AudioDataset(s1_scp='./data/M2Met/dev.scp',stage='train')
     
     dataLoader = AudioDataLoader(audio_data_trainval,batch_size=1,shuffle=True,collate_fn=collate)
 
-    
+    from AVModel import AVModel
+    model = AVModel()
+    model = nn.DataParallel(model)
+    model_path = '/Work20/2020/lijunjie/Memet/log/M2Met/model/Checkpoint_0008.pt'
+    checkpoint = torch.load(model_path)
+    model_dict = checkpoint['model']
+
+    model.load_state_dict(model_dict)
+    model.cuda()
+    model.eval()
+
+
     for idx,eg in enumerate(dataLoader):
 
-        print(eg['s1'].shape)
-        print(eg['mix'].shape)
-        print(eg['ref'].shape)
-        print(eg['s1_id'])
+        # print(eg['s1'].shape)
+        # print(eg['mix'].shape)
+        # print(eg['ref'].shape)
+        # print(eg['s1_id'])
+        mix = eg['mix'].float().cuda()
+        ref = eg['ref'].float().cuda()
+        s1 = eg['s1'].float().cuda()
+
+        # loss = cal_si_snr(s1,mix)
+        # print('%d:'%idx,loss)
+        # est,s1_emb = model([mix,ref])
+        # est = est[0].detach().cpu().permute(1, 0).numpy()
+        # est/= np.max(np.abs(est))
+        # import soundfile
+
+        # soundfile.write('./audio/%d_est.wav'%idx, est, samplerate=16000)
 
         from scipy.io import wavfile
         s1_data = np.array(eg['s1']*32767)
-        wavfile.write('./s1.wav',16000,s1_data.astype(np.int16))
+        wavfile.write('./audio/%d_s1.wav'%idx,16000,s1_data.astype(np.int16))
         mix_data = np.array(eg['mix']*32767)
-        wavfile.write('./mix.wav',16000,mix_data.astype(np.int16))
+        wavfile.write('./audio/%d_mix.wav'%idx,16000,mix_data.astype(np.int16))
         ref = np.array(eg['ref']*32767)
-        wavfile.write('./ref.wav',16000,ref.astype(np.int16))
+        wavfile.write('./audio/%d_ref.wav'%idx,16000,ref.astype(np.int16))
         # if idx>=10:
+        #     break
         break
         print('-----------')
         # print(eg['s1_ref'].size())
